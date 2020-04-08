2813387394aa15cf7e23cb5d3701dc6224a11538
stmosher <smosher2005@yahoo.com>
2020-04-08 10:05:56 -0400

    updated jenkinsfile set defaults in config file

--- config.py
+++ config.py
@@ -5,21 +5,21 @@ import os
 
 class DefaultConfig:
     """ Bot Configuration """
-    WEB_PORT = os.environ['WEB_PORT']
-    BOT_ID = os.environ['BOT_ID']
-    BOT_NAME = os.environ['BOT_NAME']
-    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-    DIALOGUE_TIMEOUT = os.environ['DIALOGUE_TIMEOUT']
-    SECRET = os.environ['SECRET']
-    MONGO_INITDB_ROOT_USERNAME = os.environ['MONGO_INITDB_ROOT_USERNAME']
-    MONGO_INITDB_ROOT_PASSWORD = os.environ['MONGO_INITDB_ROOT_PASSWORD']
-    MONGO_SERVER = os.environ['MONGO_SERVER']
-    MONGO_PORT = os.environ['MONGO_PORT']
-    MONGO_DB_ACTIVITY = os.environ['MONGO_DB_ACTIVITY']
-    MONGO_COLLECTIONS_ACTIVITY = os.environ['MONGO_COLLECTIONS_ACTIVITY']
-    SERVER_LIST = os.environ['SERVER_LIST']
-    VIRL_USERNAME = os.environ['VIRL_USERNAME']
-    VIRL_PASSWORD = os.environ['VIRL_PASSWORD']
-    APPROVED_ORG_DOMAINS = os.environ['APPROVED_ORG_DOMAINS']
-    MONGO_DB = os.environ['MONGO_DB']
-    MONGO_COLLECTIONS = os.environ['MONGO_COLLECTIONS']
+    WEB_PORT = os.environ.get('WEB_PORT', '3000')
+    BOT_ID = os.environ.get('BOT_ID', 'abc12345')
+    BOT_NAME = os.environ.get('BOT_NAME', 'rob')
+    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', '123456789')
+    DIALOGUE_TIMEOUT = os.environ.get('DIALOGUE_TIMEOUT', '30')
+    SECRET = os.environ.get('SECRET', 'abc')
+    MONGO_INITDB_ROOT_USERNAME = os.environ.get('MONGO_INITDB_ROOT_USERNAME', 'db_admin')
+    MONGO_INITDB_ROOT_PASSWORD = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', 'password123')
+    MONGO_SERVER = os.environ.get('MONGO_SERVER', 'server.example.com')
+    MONGO_PORT = os.environ.get('MONGO_PORT', '27017')
+    MONGO_DB_ACTIVITY = os.environ.get('MONGO_DB_ACTIVITY', 'bot_activity_db')
+    MONGO_COLLECTIONS_ACTIVITY = os.environ.get('MONGO_COLLECTIONS_ACTIVITY', 'bot_activity_collection')
+    SERVER_LIST = os.environ.get('SERVER_LIST', 'cml.example.com')
+    VIRL_USERNAME = os.environ.get('VIRL_USERNAME', 'admin')
+    VIRL_PASSWORD = os.environ.get('VIRL_PASSWORD', 'adminpwd')
+    APPROVED_ORG_DOMAINS = os.environ.get('APPROVED_ORG_DOMAINS', 'example.com')
+    MONGO_DB = os.environ.get('MONGO_DB', 'bot_db')
+    MONGO_COLLECTIONS = os.environ.get('MONGO_COLLECTIONS', 'bot_collections')
