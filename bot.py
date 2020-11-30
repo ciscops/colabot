f412f4838fc61433956c6580f4af2914d2f13a41
Steven Mosher <smosher2005@yahoo.com>
2020-11-30 13:20:25 -0500

    added gitlab to help

--- bot.py
+++ bot.py
@@ -23,6 +23,8 @@ mongo_url = 'mongodb://' + CONFIG.MONGO_INITDB_ROOT_USERNAME + ':' + CONFIG.MONG
 help_menu_list = ['**Create accounts** > create COLAB accounts\n',
                   '**Create AWS account** > create AWS COLAB account\n',
                   '**Create VPN account** > create an AnyConnect to COLAB VPN account\n',
+                  '**Build GitLab** > create GitLab deployment\n',
+                  '**Terminate GitLab** > terminate GitLab deployment\n',
                   '**Delete accounts** > delete COLAB accounts\n',
                   '**Reset passwords** > resets all COLAB associated passwords\n',
                   '**CML delete lab** > delete lab\n',
