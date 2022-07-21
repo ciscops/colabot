c8d19dd63e78be8a89608e576372c6b515c9b225
Paul <ppajersk@cisco.com>
2022-07-21 16:05:28 -0400

    Rewriting k8s_check

--- k8s_check.sh
+++ k8s_check.sh
@@ -1,15 +1,25 @@
-#!/bin/sh
+#!/bin/bash -xv
 
-kubectl get secret colabot-dev-1-secret
-status=$?
-if [ $status -eq 0] ; then
+# kubectl get secret colabot-dev-1-secret
+# echo $?
+# if [ $? -eq 0] ; then
+#   kubectl delete -f output-secrets-dev.yaml
+# fi
+# kubectl create -f output-secrets-dev.yaml
+#
+# kubectl get deployment colabot-dev-1
+# echo $?
+# if [ $? -eq 0] ; then
+#   kubectl delete -f output-manifest-dev.yaml
+# fi
+# kubectl create -f output-manifest-dev.yaml
+
+if [ $(kubectl get secret colabot-dev-1-secret;echo $?) ]; then
   kubectl delete -f output-secrets-dev.yaml
 fi
 kubectl create -f output-secrets-dev.yaml
 
-kubectl get deployment colabot-dev-1
-status=$?
-if [ $status -eq 0] ; then
+if [ $(kubectl get deployment colabot-dev-1;echo $?) ]; then
   kubectl delete -f output-manifest-dev.yaml
 fi
 kubectl create -f output-manifest-dev.yaml
