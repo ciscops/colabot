ff0e995135e623cbdedc9970c6f28373463d47d0
Paul <ppajersk@cisco.com>
2022-07-21 16:14:53 -0400

    Adjusting k8s file again

--- k8s_check.sh
+++ k8s_check.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -xv
 kubectl get secret colabot-dev-1-secret
 echo $?
-if [ $? -eq 0] ; then
+if [ $? -eq 0 ] ; then
   kubectl delete -f output-secrets-dev.yaml
 fi
 kubectl create -f output-secrets-dev.yaml
 
-# kubectl get deployment colabot-dev-1
-# echo $?
-#
+kubectl get deployment colabot-dev-1
+echo $?
+if [ $? -eq 0 ] ; then
+  kubectl delete -f output-manifest-dev.yaml
+fi
+kubectl create -f output-manifest-dev.yaml
+
+
 # if [ $(kubectl get secret colabot-dev-1-secret;echo $?) ]; then
 #   kubectl delete -f output-secrets-dev.yaml
 # fi
