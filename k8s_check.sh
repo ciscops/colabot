3858329930ca1cf389ed5520388a0c195bd3d338
Paul <ppajersk@cisco.com>
2022-07-21 16:12:36 -0400

    Rebuilding k8s_check

--- k8s_check.sh
+++ k8s_check.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -xv
 kubectl get secret colabot-dev-1-secret
 echo $?
-kubectl get deployment colabot-dev-1
-echo $?
+if [ $? -eq 0] ; then
+  kubectl delete -f output-secrets-dev.yaml
+fi
+kubectl create -f output-secrets-dev.yaml
+
+# kubectl get deployment colabot-dev-1
+# echo $?
 #
 # if [ $(kubectl get secret colabot-dev-1-secret;echo $?) ]; then
 #   kubectl delete -f output-secrets-dev.yaml
