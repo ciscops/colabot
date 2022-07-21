57e3397dd9827d66c6a55b4b74423409e93765c3
Paul <ppajersk@cisco.com>
2022-07-21 16:30:08 -0400

    Make pylint and removing comments

--- process-j2.py
+++ process-j2.py
@@ -2,25 +2,18 @@ import os
 from jinja2 import Template
 
 envars = os.environ
-env_dict = dict()
+env_dict = {}
 for k,v in envars.items():
     env_dict[k] = v
-print(os.getcwd())
 
-
-with open('colabot-secrets-dev.yaml.j2', 'r') as f:
+with open('colabot-secrets-dev.yaml.j2', 'r', encoding="utf8") as f:
     template = Template(f.read())
 
-with open('output-secrets-dev.yaml', 'w') as f:
+with open('output-secrets-dev.yaml', 'w', encoding="utf8") as f:
     f.write(template.render(env_dict))
 
-with open('colabot-manifest-dev.yaml.j2', 'r') as f:
+with open('colabot-manifest-dev.yaml.j2', 'r', encoding="utf8") as f:
     template = Template(f.read())
 
-with open('output-manifest-dev.yaml', 'w') as f:
+with open('output-manifest-dev.yaml', 'w', encoding="utf8") as f:
     f.write(template.render(env_dict))
-
-print(os.listdir())
-print(env_dict['BOT_NAME_DEV'])
-for i in env_dict['BOT_NAME_DEV']:
-    print(i)
