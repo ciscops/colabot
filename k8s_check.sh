39698e232f7ced160506f0f68facb570cf2ed3b4
Paul <ppajersk@cisco.com>
2022-07-21 16:19:49 -0400

    Reverting changes to workflow

--- k8s_check.sh
+++ k8s_check.sh
@@ -12,14 +12,3 @@ if [ $? -eq 0 ] ; then
   kubectl delete -f output-manifest-dev.yaml
 fi
 kubectl create -f output-manifest-dev.yaml
-
-
-# if [ $(kubectl get secret colabot-dev-1-secret;echo $?) ]; then
-#   kubectl delete -f output-secrets-dev.yaml
-# fi
-# kubectl create -f output-secrets-dev.yaml
-#
-# if [ $(kubectl get deployment colabot-dev-1;echo $?) ]; then
-#   kubectl delete -f output-manifest-dev.yaml
-# fi
-# kubectl create -f output-manifest-dev.yaml
