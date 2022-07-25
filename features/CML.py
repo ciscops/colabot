62147681c3b4f88e34be9d1984b4d337533c3876
Paul <ppajersk@cisco.com>
2022-07-25 14:25:06 -0400

    Changing response code

--- features/CML.py
+++ features/CML.py
@@ -299,7 +299,7 @@ class CML:
                     method="PUT", url=u, headers=headers, ssl=False
                 ) as res:
                     response_content = await res.json()
-                    if res.status == 200:
+                    if res.status == 204:
                         self.status_code = res.status
                         self.result = response_content
                         return True
