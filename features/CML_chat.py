ca98b7e0d2bf7909ab92df39da58fcbecacdda2c
Paul <ppajersk@cisco.com>
2022-07-25 22:52:27 -0400

    Adding correct time formatting

--- features/CML_chat.py
+++ features/CML_chat.py
@@ -148,7 +148,7 @@ async def list_all_labs(cml_servers, activity):
                 logging.debug("This is i: ")
                 logging.debug(i)
                 labs_flag = True
-                created_seconds = datetime.datetime.strptime(cml.diagnostics["labs"][i]["created"], "%Y:%m:%dT%H:%M:%S.%f%z").time()
+                created_seconds = datetime.fromisoformat(cml.diagnostics["labs"][i]["created"])
                 delta = epoch_time_now - created_seconds
                 days = int(delta // 86400)
                 hours = int(delta // 3600 % 24)
