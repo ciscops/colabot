bb8cfe4e7181aafa43ca9fbbca468055f5160375
Paul <ppajersk@cisco.com>
2022-07-25 23:19:23 -0400

    Adding datetime to epoch time

--- features/CML_chat.py
+++ features/CML_chat.py
@@ -148,7 +148,7 @@ async def list_all_labs(cml_servers, activity):
                 logging.debug("This is i: ")
                 logging.debug(i)
                 labs_flag = True
-                created_seconds = datetime.fromisoformat(cml.diagnostics["labs"][i]["created"])
+                created_seconds = int(datetime.fromisoformat(cml.diagnostics["labs"][i]["created"]).total_seconds())
                 delta = epoch_time_now - created_seconds
                 days = int(delta // 86400)
                 hours = int(delta // 3600 % 24)
