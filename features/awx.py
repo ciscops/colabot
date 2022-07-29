7745130cde6479ea79d98654e1a8906192c1a9e8
Paul <ppajersk@cisco.com>
2022-07-29 14:15:12 -0400

    Adding spaces

--- features/awx.py
+++ features/awx.py
@@ -262,11 +262,11 @@ async def create_aws_key(activity):
 
     # If the user has active access keys that colabot has not expired, don't create keys
     if len(access_key_list) > 0:
-        key_message = "```"
+        key_message = ""
         for key in access_key_list:
-            key_message += f"Key id: {key.access_key_id} | Status: {key.status} | Created: {key.create_date}  "
-            key_message += "\n"
-        
+            key_message += f"    Key id: {key.access_key_id} | Status: {key.status} | Created: {key.create_date} \n"
+            #key_message += "\n"
+
         message = dict(
             text=(
                 "You already have active aws keys: \n"
