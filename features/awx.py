7980525460b8c436d88c85d0f99b47669d2662c1
Paul <ppajersk@cisco.com>
2022-07-29 13:05:51 -0400

    Adding indent

--- features/awx.py
+++ features/awx.py
@@ -264,13 +264,13 @@ async def create_aws_key(activity):
     if len(access_key_list) > 0:
         key_message = ""
         for key in access_key_list:
-            key_message += f"``` Key id: {key.access_key_id} | Status: {key.status} | Created: {key.create_date} ```\n"
+            key_message += f" Key id: {key.access_key_id} | Status: {key.status} | Created: {key.create_date} \n"
 
         message = dict(
             text=(
-                "You already have active aws keys:\n"
+                "You already have active aws keys: ```\n"
                 + key_message
-                + "If you would like to refresh them, use **reset aws keys**"
+                + "```If you would like to refresh them, use **reset aws keys**"
             ),
             toPersonId=activity["sender"],
         )
