caba72136865d338418b6023981508d52fadd3d1
Kristopher Stickney <kstickne@cisco.com>
2022-08-31 21:48:22 -0400

    removed testing code

--- awslambda/lambda_function_cml.py
+++ awslambda/lambda_function_cml.py
@@ -14,13 +14,10 @@ def lambda_handler(event, handle):
     start_time = datetime.datetime.now()
 
     cml = CMLManager()
-    # success_count, fail_count = cml.manage_labs()
+    success_count, fail_count = cml.manage_labs()
 
-    # logger.debug("Succesful user iterations: %d", success_count)
-    # logger.debug("Failed user iterations: %d", fail_count)
-    cml.wipe_lab()
-
-
+    logger.debug("Succesful user iterations: %d", success_count)
+    logger.debug("Failed user iterations: %d", fail_count)
 
     end_time = datetime.datetime.now()
     logger.debug('Script complete, total runtime {%s - %s}', end_time, start_time)
