1cbf683c9f9c37262a794d10baabd9d4b35a9cf3
Paul <>
2022-09-16 15:57:37 -0400

    adding correct email parse

--- features/awx.py
+++ features/awx.py
@@ -659,7 +659,7 @@ async def wipe_and_delete_labs(activity, labs, user_email, table):
     """Wipes the labs, sends the user each lab's yaml file, and messages the user"""
     cml_server = CONFIG.SERVER_LIST.split(",")[0]
     logging.debug("CML Server is: %s", cml_server)
-    user_and_domain = activity["sender_email"].split("@")
+    user_and_domain = user_email.split("@")
     cml_user = CML(user_and_domain[0], activity["cml_password"], cml_server)
 
     for lab_id in labs:
