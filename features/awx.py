f32311646fbff9ab75e916344c5a191771f28de0
Paul <>
2022-10-10 17:30:51 -0400

    adding cypher to dev yaml

--- features/awx.py
+++ features/awx.py
@@ -686,7 +686,7 @@ async def get_cml_password(user_email, table):
     #fernet_cypher_bytes = bytes(, 'utf-8')
 
     # cipher = base64.urlsafe_b64encode(fernet_cypher_bytes)
-    fernet_decrypt = Fernet(CONFIG.AWX_DECRYPT_KEY)
+    fernet_decrypt = Fernet(CONFIG.COLABOT_CYPHER)
     decrypted_key = fernet_decrypt.decrypt(cml_password)
     key = decrypted_key.decode()
 
